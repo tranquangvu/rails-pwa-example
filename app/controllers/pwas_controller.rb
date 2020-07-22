10f7561354cf669c87f21db7d52b58b5780232f3
tranquangvu <vutran.513@gmail.com>
2020-07-22 11:28:45 +0700

    Handle pwa for rails

---
+++ app/controllers/pwas_controller.rb
@@ -0,0 +1,7 @@
+class PwasController < ApplicationController
+  protect_from_forgery except: :service_worker
+
+  def service_worker; end
+
+  def manifest; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/service-worker.js', to: 'pwas#service_worker'
+  get '/manifest.json', to: 'pwas#manifest'
+
   root 'home#index'
 end
